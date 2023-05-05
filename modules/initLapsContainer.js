3885c82c9ae19a8adbbee4ad39880bc8f273193d
mahim <mahim216@gmail.com>
2023-05-05 00:36:14 -0400

    Create initLapsContainer.js

---
+++ modules/initLapsContainer.js
@@ -0,0 +1,4 @@
+export default function initLapsContainer() {
+    document.querySelector('aside').classList.remove('hidden');
+    document.querySelector('.container').classList.add('side-by-side');
+}
